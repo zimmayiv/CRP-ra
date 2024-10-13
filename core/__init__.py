9c8086fadd3b2db1e2351b6d6dee067fe1e3e0f9
Peter Xu <yiv@Peters-MacBook-Pro-3.local>
2024-10-13 01:24:45 -0700

    add logic for weird sqlalchemy url pickiness

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,12 @@ from sqlalchemy import Table
 
 from core import views
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
+database_url = os.environ.get('DATABASE_URL')
+
+if database_url and database_url.startswith('postgres:'):
+    database_url = database_url.replace('postgres:', 'postgresql:', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url 
 # 'postgres://ua0r6rukoilnjm:p4ba19785c7f62b33ec59fa4fdca3a4a4f2c372306df42a2f354266f76b4d9b28@caij57unh724n3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7kfescnbc07c3'
 # 'postgresql://yiv:postgres@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
