c04ff919d7227e4c1f94ec7ec83603a9984e6729
Peter Xu <yiv@Peters-MacBook-Pro-3.local>
2024-10-12 18:28:00 -0700

    forgot quotes

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy import Table
 
 from core import views
 
-app.config['SQLALCHEMY_DATABASE_URI'] = postgres://ua0r6rukoilnjm:p4ba19785c7f62b33ec59fa4fdca3a4a4f2c372306df42a2f354266f76b4d9b28@caij57unh724n3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7kfescnbc07c3
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ua0r6rukoilnjm:p4ba19785c7f62b33ec59fa4fdca3a4a4f2c372306df42a2f354266f76b4d9b28@caij57unh724n3.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7kfescnbc07c3'
 # os.environ.get('DATABASE_URL') # 'postgresql://yiv:postgres@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
